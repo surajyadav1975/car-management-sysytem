6e1eb45d96f1fd2898bbe908cbb2fbfac77d3cde
surajyadav1975fk <ysuraj1191@gmail.com>
2024-11-14 20:25:51 +0530

    changed frontend link in cors

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieparser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://car-management-sysytem-alvu.vercel.app',
     credentials: true, 
     methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
